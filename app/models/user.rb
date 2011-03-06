60068347c5e5c7a1ba0264133269d3d073983aea
hbarradah <hbarradah78@gmail.com>
2011-03-06 18:52:18 -0500

    Finsihed first cut of the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
 
-  validates :name, :presence => true
-  validates :email, :presence => true
+  validates :name, :presence => true,
+                   :length => { :maximum => 50 }
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, :presence => true,
+                    :format => { :with => email_regex},
+                    :uniqueness => { :case_sensitive => false}
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,44 @@ describe User do
     no_email_user = User.new(@attr.merge(:email => ""))
     no_email_user.should_not be_valid
   end
+
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new (@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+
+  it "should accept valid email addresses" do
+    addresses = %w[user@info.com user@foo.bar.org first.last@foo.org]
+
+    addresses.each do |address|
+      valid_email_address  = User.new(@attr.merge(:email => address))
+      valid_email_address.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    addresses = %w[user@info,com user_foo_bar.org first.last@foo]
+
+    addresses.each do |address|
+      valid_email_address  = User.new(@attr.merge(:email => address))
+      valid_email_address.should_not be_valid
+    end
+  end
+
+  it "should reject duplicate email addresses" do
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+
+    user_with_duplicate_email.should_not be_valid
+  end
+
+  it "should reject identical email addresses up to case" do
+    upcased_email = @attr[:email].upcase
+
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
+
+    user_with_duplicate_email.should_not be_valid
+  end
 end
